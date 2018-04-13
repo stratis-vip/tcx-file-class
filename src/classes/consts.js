83fb7874e77fdebb9542fc65d5c37dbc87e957e5
stratis <stratis.vip@gmail.com>
2018-04-13 18:31:50 +0300

    deleted uneeded files

--- src/classes/consts.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var ERROR_NUMBER_VALUE = -1;
-exports.ERROR_NUMBER_VALUE = ERROR_NUMBER_VALUE;
-var ERROR_STRING_VALUE = "";
-exports.ERROR_STRING_VALUE = ERROR_STRING_VALUE;
