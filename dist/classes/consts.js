2e62a08d02e74a0fcbf687ac6348f613892261a6
stratis <stratis.vip@gmail.com>
2018-04-17 18:07:32 +0300

    added function getExt

--- dist/classes/consts.js
+++ dist/classes/consts.js
@@ -4,4 +4,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.ERROR_NUMBER_VALUE = -1;
 /**Λάθος τιμή για γραμματοσειρά */
 exports.ERROR_STRING_VALUE = "";
+function getExt(arg) {
+    if (arg !== undefined) {
+        return Number(arg[0]);
+    }
+    else {
+        return exports.ERROR_NUMBER_VALUE;
+    }
+}
+exports.getExt = getExt;
 //# sourceMappingURL=consts.js.map
--- src/classes/consts.ts
+++ src/classes/consts.ts
@@ -2,3 +2,13 @@
 export const ERROR_NUMBER_VALUE = -1;
 /**Λάθος τιμή για γραμματοσειρά */
 export const ERROR_STRING_VALUE = "";
+
+
+export function getExt<T>(arg: T): number {
+    if (arg !== undefined) {
+        return Number((arg[0] as Array<string>));
+    }
+    else {
+        return ERROR_NUMBER_VALUE;
+    }
+}
